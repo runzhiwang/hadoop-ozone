e6b05faf55f8c04090ccaf2769c504195e3c6427
Márton Elek <elek@apache.org>
2019-11-27 14:24:25 +0100

    debug-acceptace-timeout

---
+++ .github/workflows/tmate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get install tmate
+tmate -S /tmp/tmate.sock new-session -d
+tmate -S /tmp/tmate.sock wait tmate-ready
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
