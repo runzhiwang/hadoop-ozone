105256471cc1389649432984f1e96d74dd0848c6
Márton Elek <elek@apache.org>
2019-11-27 20:01:58 +0100

    try to fix tmate

--- .github/workflows/tmate.sh
+++ .github/workflows/tmate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -ex
 sudo apt-get -y install tmate
 tmate -S /tmp/tmate.sock new-session -d
-tmate -S /tmp/tmate.sock wait tmate-ready
+sleep 2
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
