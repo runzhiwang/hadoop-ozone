7a492c351a4bbf7bec8c9c0b1644b03479b2932a
runzhiwang <runzhiwang@tencent.com>
2020-06-18 20:11:16 +0800

    fix ut

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/container/common/statemachine/commandhandler/TestBlockDeletion.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/container/common/statemachine/commandhandler/TestBlockDeletion.java
@@ -332,7 +332,7 @@ public class TestBlockDeletion {
             .build().getDBByteKey();
         Assert.assertNotNull(db.getStore().get(
             RocksDB.DEFAULT_COLUMN_FAMILY,
-            key);
+            key));
       }
     }, omKeyLocationInfoGroups);
   }
