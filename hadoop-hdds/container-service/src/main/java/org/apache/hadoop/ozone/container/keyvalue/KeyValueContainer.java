4babb586a6ccf734a29419fa297186d9d576291e
runzhiwang <runzhiwang@tencent.com>
2020-06-30 11:14:32 +0800

    revert setDBPath

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueContainer.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueContainer.java
@@ -140,8 +140,8 @@ public class KeyValueContainer implements Container<KeyValueContainerData> {
       //Set containerData for the KeyValueContainer.
       containerData.setChunksPath(chunksPath.getPath());
       containerData.setContainerDBType(impl);
-      containerData.setDbPath(dbCategory.getDbPath());
-      containerData.setCategoryInDB(dbCategory.getCategoryInDB());
+      containerData.setDbPath(dbFile.getAbsolutePath());
+      containerData.setCategoryInDB(RocksDB.DEFAULT_COLUMN_FAMILY);
       containerData.setVolume(containerVolume);
 
       // Create .container file
